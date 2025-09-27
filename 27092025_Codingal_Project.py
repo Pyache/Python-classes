872b543e4c592c0488f696d742193c456a4bb1e1
Pyache <talin.chauhan@gmail.com>
2025-09-27 20:25:12 +0530

    Add files via upload

--- 27092025_Codingal_Project.py
+++ 27092025_Codingal_Project.py
@@ -1,8 +1,8 @@
-x = int(input("Enter the radius of the circle (in cm): "))
-def circumpherence(R):
+x = float(input("Enter the radius of the circle (in cm): "))
+def circumference(R):
     return R * 3.1428576 * 2
 if x > 0:
-    print("The circumpherence of the circle is approximately",circumpherence(x),"cm.")
+    print("The circumference of the circle is approximately",circumference(x),"cm.")
 else:
     print("The radius you gave,", x, ",is invalid")
 

894833125629601e6eaa311a10513f624c826352
Pyache <talin.chauhan@gmail.com>
2025-10-25 20:08:38 +0530

    Add files via upload

--- 25102025_Sets-and-Arrays_Codingal_Project.py
+++ 25102025_Sets-and-Arrays_Codingal_Project.py
@@ -3,14 +3,16 @@ seta2 = {"blue", "yellow"}
 symmetric_dif1 = seta1.symmetric_difference(seta2)
 union1 = seta1.union(seta2)
 diff1 = seta1.difference(seta2)
+print("Original sets: \n", seta1, "\n", seta2, "\n")
 print(symmetric_dif1)
 print(union1)
-print(diff1, "\n")
+print(diff1, "\n\n")
 setb1 = {1, 2, 3, 4, 5}
 setb2 = {1, 5, 6, 7, 8, 9}
 symmetric_dif2 = setb1.symmetric_difference(setb2)
 union2 = setb1.union(setb2)
 diff2 = setb1.difference(setb2)
+print("Original sets: \n", setb1, "\n", setb2, "\n")
 print(symmetric_dif2)
 print(union2)
 print(diff2)
